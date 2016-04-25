cde340b8e3ee787041160457c62ab5f5c03c7bde
Denys Sidorov <000scorpions000@gmail.com>
2016-04-25 19:44:46 +0300

    Create Servlets - editUser, createUser,DeleteUser, add some JSPs

--- src/main/java/ru/clinicPetWeb/servlets/ClientViewServlet.java
+++ src/main/java/ru/clinicPetWeb/servlets/ClientViewServlet.java
@@ -11,13 +11,14 @@ import java.io.IOException;
 
 public class ClientViewServlet extends HttpServlet {
 
-	private final ClientCache CLIENT_CACHE = ClientCache.getInstance();
+		private final ClientCache CLIENT_CACHE = ClientCache.getInstance();
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		req.setAttribute("clients", this.CLIENT_CACHE.values());
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/views/client/ClientView.jsp");
-		dispatcher.forward(req, resp);
-	}
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			req.setCharacterEncoding("UTF-8");
+			req.setAttribute("clients", this.CLIENT_CACHE.values());
+
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/views/client/ClientView.jsp");
+			dispatcher.forward(req, resp);
+		}
 }
